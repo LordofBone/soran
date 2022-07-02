5a45ebbdc3802172e457a36831aa5af9d32ed9ee
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-07-02 20:48:35 +0100

    Changed to split out the listening and inferencing parts so that an application importing this can display listening/inferencing start/finish information clearly.
    Also made it an importable class that contains veriables for whether this is listening/inferencing, again to make it easier for other applications using this to check whether the program is listening/inferencing.

--- integrate_stt.py
+++ integrate_stt.py
@@ -4,15 +4,38 @@ from functions.speech_input import listen
 import logging
 
 
-def run_stt_inference():
-    """
-    Run speech inference.
-    :return:
-    """
-    listen()
-    return SpeechInferencer.run_stt()
+class SpeechtoTextHandler:
+    def __init__(self):
+        self.listening = False
+        self.inferencing = False
+
+    def initiate_recording(self):
+        """
+        Initiate recording.
+        :return:
+        """
+        self.listening = True
+
+        listen()
+
+        self.listening = False
+
+    def run_inference(self):
+        """
+        Run speech inference.
+        :return:
+        """
+        self.inferencing = True
+
+        text_output = SpeechInferencer.run_stt()
+
+        self.inferencing = False
+
+        return text_output
 
 
 if __name__ == '__main__':
     logging.basicConfig(level="DEBUG")
-    print(run_stt_inference())
+    SpeechtoTextTest = SpeechtoTextHandler()
+    SpeechtoTextTest.initiate_recording()
+    print(SpeechtoTextTest.run_inference())
